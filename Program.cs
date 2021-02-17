bfa3626a7ed74a1d30feb7ca5d5d4f441f823653
Jaahhh <james.ahfat@gmail.com>
2021-02-17 12:54:36 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Smart_Direct_Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
