82faf963b85e76035e4d11548dd5914e65ce2625
Jaahhh <james.ahfat@gmail.com>
2021-02-17 20:23:14 -0700

    migrate to net472

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
-﻿using System;
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+using Topshelf;
 
 namespace Smart_Direct_Service
 {
     class Program
     {
+        public static readonly ILog _log = LogManager.GetLogger(typeof(SmartDirectService));
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            HostFactory.Run(hostConfig =>
+            {
+                var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+                XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+
+                hostConfig.UseLog4Net("log4net.config");
+
+                hostConfig.Service<SmartDirectService>(serviceConfig =>
+                {
+                    serviceConfig.ConstructUsing(() => new SmartDirectService());
+                    serviceConfig.WhenStarted(s => s.Start());
+                    serviceConfig.WhenStopped(s => s.Stop());
+                });
+                hostConfig.RunAsLocalSystem();
+                hostConfig.SetDescription("Service to watch directory for incoming file input and perfrom interactions with Web API");
+                hostConfig.SetDisplayName("Smart Direct Messager");
+                hostConfig.SetServiceName("SDSMessager");
+            });
         }
     }
 }
---
+++ SmartDirectService.cs
@@ -0,0 +1,93 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+using log4net;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Smart_Direct_Service
+{
+    class SmartDirectService
+    {
+        public static readonly ILog _log = LogManager.GetLogger(typeof(SmartDirectService));
+        private FileSystemWatcher CsvWatcher;
+        private string watchDirectory = "C:\\smartsms\\";
+
+
+        public void Start()
+        {
+            _log.Info("Starting up SDS");
+            RunWatcher();
+        }
+
+        public void Stop()
+        {
+            CsvWatcher.Dispose();
+            _log.Info("Stopping SDS");
+        }
+
+        private void RunWatcher()
+        {
+            if (!Directory.Exists(watchDirectory))
+            {
+                Directory.CreateDirectory(watchDirectory);
+            }
+            CsvWatcher = new FileSystemWatcher
+            {
+                Path = watchDirectory,
+                //must have LastWrite and FileName to detect created events
+                NotifyFilter = NotifyFilters.FileName | NotifyFilters.LastWrite,
+                Filter = "*.csv",
+            };
+
+            CsvWatcher.Created += new FileSystemEventHandler(processCSV);
+            CsvWatcher.EnableRaisingEvents = true;
+            _log.Debug("Watcher Initiated");
+        }
+
+        private void processCSV(object sender, FileSystemEventArgs e)
+        {
+            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
+            using (var reader = new StreamReader(e.FullPath))
+            using (var csv = new CsvReader(reader, config))
+            {
+                var record = new Fields();
+                var records = csv.EnumerateRecords(record);
+                foreach (var r in records)
+                {
+                    CallExternalAPI(r.Phone, r.Message);
+                }
+            }
+        }
+
+        private static void CallExternalAPI(string phone, string message)
+        {
+            var param = new { sender = "6714320", text = message, key = "29c16dee9f79234513e0c85d4357ae6c", receiver = phone };
+
+            var client = new RestClient("https://direct.smart.bz/api/P2001");
+
+            var request = new RestRequest()
+                .AddJsonBody(param);
+
+            var reponse = client.Post(request);
+
+            _log.Debug(reponse.Content);
+        }
+    }
+
+    public class Fields
+    {
+        [Index(1)]
+        public string Message { get; set; }
+
+        [Index(0)]
+        public string Phone { get; set; }
+    }
+}
