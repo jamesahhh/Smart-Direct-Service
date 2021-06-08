01b6e4659b460e6063441c8df164f6fe77590000
Jaahhh <james.ahfat@gmail.com>
2021-06-08 14:06:35 -0600

    add fileLock check and new config source

--- SmartDirectService.cs
+++ SmartDirectService.cs
@@ -6,9 +6,11 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Diagnostics;
 using System.Globalization;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace Smart_Direct_Service
 {
@@ -20,6 +22,7 @@ namespace Smart_Direct_Service
         private string key = ConfigurationManager.AppSettings["APIsecret"];
         private string sender = ConfigurationManager.AppSettings["sender"];
         private string apiurl = ConfigurationManager.AppSettings["Apiurl"];
+        private string outDirectory = ConfigurationManager.AppSettings["processedDirectory"];
 
         public void Start()
         {
@@ -54,20 +57,30 @@ namespace Smart_Direct_Service
 
         private void processCSV(object sender, FileSystemEventArgs e)
         {
+            do
+            {
+                Thread.Sleep(100);
+            } while (!IsFileClosed(e.FullPath, true));
             var config = new CsvConfiguration(CultureInfo.InvariantCulture)
             {
                 HasHeaderRecord = false,
             };
-            using (var reader = new StreamReader(e.FullPath))
-            using (var csv = new CsvReader(reader, config))
+            using (StreamReader reader = new StreamReader(e.FullPath))
             {
-                var record = new Fields();
-                var records = csv.EnumerateRecords(record);
-                foreach (var r in records)
+                using (CsvReader csv = new CsvReader(reader, config))
                 {
-                    CallExternalAPI(r.Phone, r.Message);
+                    var record = new Fields();
+                    var records = csv.EnumerateRecords(record);
+                    foreach (var r in records)
+                    {
+                        //_log.Info(r.Phone + r.Message);
+                        CallExternalAPI(r.Phone, r.Message);
+                    }
                 }
             }
+            File.Move(e.FullPath, outDirectory + e.Name);
+            //_log.Info("disposed");
+
         }
 
         private void CallExternalAPI(string phone, string message)
@@ -83,7 +96,41 @@ namespace Smart_Direct_Service
 
             _log.Debug(reponse.Content);
         }
+
+        public static bool IsFileClosed(string filepath, bool wait)
+        {
+            var fileClosed = false;
+            var retries = 20;
+            const int delay = 500; // Max time spent here = retries*delay milliseconds
+
+            if (!File.Exists(filepath))
+                return false;
+
+            do
+            {
+                try
+                {
+                    // Attempts to open then close the file in RW mode, denying other users to place any locks.
+                    var fs = File.Open(filepath, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+                    fs.Close();
+                    fileClosed = true; // success
+                }
+                catch (IOException) { }
+
+                if (!wait) break;
+
+                retries--;
+
+                if (!fileClosed)
+                    Thread.Sleep(delay);
+            }
+            while (!fileClosed && retries > 0);
+
+            return fileClosed;
+        }
     }
+
+
 
     public class Fields
     {
